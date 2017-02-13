5df38c5915fba602c3abab4aa38db126ea2dd748
alessandroboaro <alessandro.boaro@gmail.com>
2017-02-13 22:01:07 +0100

    second run

--- run_analysis.R
+++ run_analysis.R
@@ -8,11 +8,14 @@ data_test = scan('./test/X_test.txt', what=numeric())
 # Set columns number by labels
 data_test = matrix(data_test, ncol=length(labls), byrow=T, dimnames=list(NULL, labls))
 #Read Y
-y_test = read.csv('./test/Y_test.txt',header=FALSE,colClasses = c(factor()),col.names = c('Activity'))
+y_test = read.csv('./test/y_test.txt',header=FALSE,colClasses = c(factor()),col.names = c('Activity'))
 #Assign to Y activity names
 y_test<-join(y_test,activities,type="inner")
-#Bind Y to test set
-data_test<-cbind(y_test[2],data_test)
+#Read Subject
+subject_test = read.csv('./test/subject_test.txt',header=FALSE,col.names = c('Subject'))
+
+data_test<-cbind(subject_test,y_test[2],data_test)
+
 
 
 #Read trainset
@@ -20,11 +23,13 @@ data_train = scan('./train/X_train.txt', what=numeric())
 # Set columns number by labels
 data_train = matrix(data_train, ncol=length(labls), byrow=T, dimnames=list(NULL, labls))
 #Read Y
-y_train = read.csv('./train/Y_train.txt',header=FALSE,colClasses = c(factor()),col.names = c('Activity'))
+y_train = read.csv('./train/y_train.txt',header=FALSE,colClasses = c(factor()),col.names = c('Activity'))
 #Assign to Y activity names
 y_train<-join(y_train,activities,type="inner")
-#Bind Y to train set
-data_train<-cbind(y_train[2],data_train)
+#Read Subject
+subject_train = read.csv('./train/subject_train.txt',header=FALSE,col.names = c('Subject'))
+
+data_train<-cbind(subject_train,y_train[2],data_train)
 
 
 # Merge data sets
@@ -35,8 +40,8 @@ means<-lapply(full_dataset[2:ncol(full_dataset)],mean)
 #Standard Deviation of measures
 sds<-lapply(full_dataset[2:ncol(full_dataset)],sd)
 
-#Means by activity
-activity_mean<-aggregate(.~Activity_desc, data=full_dataset, mean)
+#Means by activity and subject
+activity_mean<-aggregate(.~Activity_desc+Subject, data=full_dataset, mean)
 #SD by activity
 activity_sd<-aggregate(.~Activity_desc, data=full_dataset, sd)
 
